9e2659f926dde4cf5cb33f40d055eff7eddb6fef
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-08-31 12:40:04 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+function handleRoomService(req, res) {
+    if (!isLoggedIn(req)) {
+      return res.send(401)
+    }
+  
+    const API_KEY = "proj_56729923-0e35-4bc6-81be-3eaac9b06e6a    "
+    const user = "this-user-id"
+    const body = req.body
+  
+    const r = await fetch('https://super.roomservice.dev/provision', {
+      method: 'post',
+      headers: {
+        Authorization: `Bearer: ${API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: user,
+        resources: body,
+      }),
+    });
+  
+    return res.json(await r.json())
+  }
